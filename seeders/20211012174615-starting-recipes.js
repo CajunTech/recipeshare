c3a552ad75af874187ff7c7e264cdb9c2c607105
Jeremy Ledet <LedetJeremyJ@JohnDeere.com>
2021-10-13 00:56:16 -0500

    added seed data

--- seeders/20211012174615-starting-recipes.js
+++ seeders/20211012174615-starting-recipes.js
@@ -177,6 +177,20 @@ module.exports = {
       img: `https://www.themealdb.com/images/media/meals/uyqrrv1511553350.jpg`, 
       cuisine:`British`,
      },
+     {
+      name: 'Good Old Fashioned Pancakes',
+      ingredients:`1 ½ cups all-purpose flour
+      3 ½ teaspoons baking powder
+      1 teaspoon salt
+      1 tablespoon white sugar
+      1 ¼ cups milk
+      1 egg
+      3 tablespoons butter, melted`,
+      instructions:`n a large bowl, sift together the flour, baking powder, salt and sugar. Make a well in the center and pour in the milk, egg and melted butter; mix until smooth.
+      Heat a lightly oiled griddle or frying pan over medium-high heat. Pour or scoop the batter onto the griddle, using approximately 1/4 cup for each pancake. Brown on both sides and serve hot.`,
+      img: `https://www.inspiredtaste.net/wp-content/uploads/2016/07/Pancake-Recipe-2-1200.jpg`, 
+      cuisine:`American`,
+     },
   ], {});
 
   },
